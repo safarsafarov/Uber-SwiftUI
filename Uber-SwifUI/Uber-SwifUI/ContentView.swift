9f05e98386146c5b40d238dc5347c0ea927ee600
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-26 23:39:51 +0500

    ✨

---
+++ Uber-SwifUI/Uber-SwifUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Uber-SwifUI
+//
+//  Created by Safar Safarov on 26/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Uber-SwifUI/Uber-SwifUI/Uber_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_SwifUIApp.swift
+//  Uber-SwifUI
+//
+//  Created by Safar Safarov on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Uber_SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
